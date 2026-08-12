c9e1d6e3a7174b43f8d418951b3ed98576e67af1
Adeowo <mac@Ades-mac-air.local>
2026-08-12 08:01:05 +0100

    Remove unknown create_admin command and update build.sh

--- build.sh
+++ build.sh
@@ -8,33 +8,4 @@ python -m pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-# Uses built-in Django createsuperuser non-interactively
-python manage.py createsuperuser --no-input || true
-
-
-
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
-
-python manage.py collectstatic --no-input
-python manage.py migrate
-python manage.py create_admin
-
-
-
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
-
-python manage.py collectstatic --no-input
-python manage.py migrate
-
-# Built-in Django command to create superuser without interactive prompts
 python manage.py createsuperuser --no-input || true
