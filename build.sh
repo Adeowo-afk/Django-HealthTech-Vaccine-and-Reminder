e7c073949fc533bc8e11fa25eb30efd6edd4e54a
Adeowo <mac@Ades-mac-air.local>
2026-08-12 07:53:12 +0100

    Use built-in createsuperuser command in build.sh

--- build.sh
+++ build.sh
@@ -21,3 +21,18 @@ python -m pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py create_admin
+
+
+
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py migrate
+
+# Built-in Django command to create superuser without interactive prompts
+python manage.py createsuperuser --no-input || true
