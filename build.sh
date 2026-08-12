0674834d4cf187b1f516d5d5abb082ea6b7120f2
Adeowo <mac@Ades-mac-air.local>
2026-08-12 07:43:04 +0100

    Add automatic superuser creation script

--- build.sh
+++ build.sh
@@ -8,3 +8,16 @@ python -m pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+
+
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py migrate
+python manage.py create_admin
